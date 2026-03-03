afb35ae0a101d3936ddd24a0ec132ac3756b16e1
jy <jungjuneyoung6@gmail.com>
2026-03-03 22:36:15 +0900

    feat:docker-compose-dev.yml added

--- jym-api-gateway/src/main/java/jymusic/jym_api_gateway/config/SpringSecurityConfig.java
+++ jym-api-gateway/src/main/java/jymusic/jym_api_gateway/config/SpringSecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
  */
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SpringSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
